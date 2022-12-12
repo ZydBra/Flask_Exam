eed4b721b311d9e19ba4f058f74687e2060269a8
ZydBra <z.brazionis@gmail.com>
2022-12-12 21:25:53 +0200

    Sukuriami meršrutai

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from flask_bcrypt import Bcrypt
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -67,6 +67,90 @@ def uzkrauti_naudotoja(e_pastas):
 
 with app.app_context():
     db.create_all()
+
+
+# Programos maršrutai:
+@app.route('/')
+def index():
+    return redirect(url_for('prisijungti'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def registruotis():
+    forma = forms.RegistravimosiForma()
+    if forma.validate_on_submit():
+        koduotas_slaptazodis = bcrypt.generate_password_hash(forma.slaptazodis_1.data).decode('utf-8')
+        naudotojas = Naudotojas(
+            vardas_pavarde=forma.vardas_pavarde.data,
+            e_pastas=forma.e_pastas.data,
+            slaptazodis=koduotas_slaptazodis
+        )
+        db.session.add(naudotojas)
+        db.session.commit()
+        flash(f'Registration succeeded, {naudotojas.vardas_pavarde}, now you can sign in.', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', form=forma)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def prisijungti():
+    forma = forms.PrisijungimoForma()
+    if forma.validate_on_submit():
+        naudotojas = Naudotojas.query.filter_by(e_pastas=forma.e_pastas.data).first()
+        if not naudotojas:
+            flash(f'User with email {forma.e_pastas.data} does not exist!', 'danger')
+            return redirect(url_for('prisijungti'))
+        if not bcrypt.check_password_hash(naudotojas.slaptazodis, forma.slaptazodis.data):
+            flash(f'User with email {forma.e_pastas.data} password do not match!', 'danger')
+            return redirect(url_for('prisijungti'))
+        login_user(naudotojas)
+        flash(f'Welcome, {naudotojas.vardas_pavarde}!', 'success')
+        return redirect(request.args.get('next') or url_for('atvaizduoti_grupes'))
+    return render_template('login.html', form=forma)
+
+
+@app.route('/logout')
+def atsijungti():
+    flash(f'See you next time, {current_user.vardas_pavarde}!', 'success')
+    logout_user()
+    return redirect(url_for('prisijungti'))
+
+
+@app.route('/groups')
+@login_required
+def atvaizduoti_grupes():
+    try:
+        visos_grupes = Grupe.query.filter(Grupe.naudotojai.any(Naudotojas.id == current_user.id)).all()
+    except:
+        visos_grupes = []
+    print(visos_grupes)
+    return render_template('groups.html', visos_grupes=visos_grupes)
+
+
+@app.route('/bills/<grupes_id>', methods=['GET', 'POST'])
+@login_required
+def ivesti_saskaita(grupes_id):
+    forma = forms.SaskaitosIvedimoForma()
+    # forma.query = Grupe.query.filter_by(id=current_user.id)
+    if forma.validate_on_submit():
+        saskaita = Saskaita(sask_suma=forma.sask_suma.data, aprasymas=forma.aprasymas.data, grupes_id=grupes_id)
+        db.session.add(saskaita)
+        db.session.commit()
+        flash(f'Invoice ID: {saskaita.id} added.', 'success')
+        return redirect(url_for('ivesti_saskaita', grupes_id=grupes_id))
+    return render_template('bills.html', form=forma)
+
+
+@app.route('/show_bills/<grupes_id>', methods=['GET', 'POST'])
+@login_required
+def rodyti_grupes_saskaitas(grupes_id):
+    try:
+        grupes_saskaitos = Saskaita.query.filter_by(grupes_id=grupes_id).all()
+    except:
+        grupes_saskaitos = []
+    print(grupes_saskaitos)
+    return render_template("show_bills.html", grupes_saskaitos=grupes_saskaitos, grupes_id=grupes_id)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5010)
